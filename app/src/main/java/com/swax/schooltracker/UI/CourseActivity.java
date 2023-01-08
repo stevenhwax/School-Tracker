e1c5866ebe95bc3751be1faab461b64dc05920b5
SteveWax <stevehwax@gmail.com>
2023-01-07 17:06:29 -0800

    Finished CourseActivity page and added all necessary data validation

--- app/src/main/java/com/swax/schooltracker/UI/CourseActivity.java
+++ app/src/main/java/com/swax/schooltracker/UI/CourseActivity.java
@@ -17,6 +17,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -54,10 +55,6 @@ public class CourseActivity extends AppCompatActivity implements AdapterView.OnI
     private String mCoursePhone = "425-555-1212";
     private String mCourseEmail = "teach@teacher.com";
     private String mCourseNotes = "";
-    private List<Integer> associatedTermIds = new ArrayList<>();
-    private List<Term> associatedTerms = new ArrayList<>();
-    private List<String> courseAddTermStrings;
-    private List<String> courseDeleteTermStrings;
     private List<Integer> associatedAssessmentIds = new ArrayList<>();
     private List<Assessment> associatedAssessments = new ArrayList<>();
     private List<String> courseAddAssessmentStrings;
@@ -87,6 +84,8 @@ public class CourseActivity extends AppCompatActivity implements AdapterView.OnI
             mCourse = new Course(mCourseName, mCourseStart, mCourseEnd, mCourseStatus, mCourseInstructor, mCoursePhone, mCourseEmail, mCourseNotes, associatedAssessmentIds);
         } else {
             mCourse = repo.getCourseById(getIntent().getIntExtra("id", 0));
+            mCourseStart = mCourse.getCourseStart();
+            mCourseEnd = mCourse.getCourseEnd();
         }
 
         courseNameEditText = findViewById(R.id.courseNameEditText);
@@ -153,16 +152,16 @@ public class CourseActivity extends AppCompatActivity implements AdapterView.OnI
         courseStatusSpinner.setAdapter(courseStatusAdapter);
         courseStatusSpinner.setOnItemSelectedListener(this);
 
-        EditText courseInstructorEditText = findViewById(R.id.courseInstructorEditText);
+        courseInstructorEditText = findViewById(R.id.courseInstructorEditText);
         courseInstructorEditText.setText(mCourse.getCourseInstructor());
 
-        EditText coursePhoneEditText = findViewById(R.id.coursePhoneEditText);
+        coursePhoneEditText = findViewById(R.id.coursePhoneEditText);
         coursePhoneEditText.setText(mCourse.getInstructorPhone());
 
-        EditText courseEmailEditText = findViewById(R.id.courseEmailEditText);
+        courseEmailEditText = findViewById(R.id.courseEmailEditText);
         courseEmailEditText.setText(mCourse.getInstructorEmail());
 
-        EditText courseNotesEditText = findViewById(R.id.courseNotesEditText);
+        courseNotesEditText = findViewById(R.id.courseNotesEditText);
         courseNotesEditText.setText(mCourse.getCourseNotes());
 
         populateFields();
@@ -174,22 +173,6 @@ public class CourseActivity extends AppCompatActivity implements AdapterView.OnI
         switch(parent.getId()){
             case R.id.courseStatusSpinner:
                 mCourseStatus = (String) courseStatusStrings.get(position);
-                break;
-            case R.id.courseAddTermSpinner:
-                if (position != 0){
-                    Term courseAddTermSelected = repo.getTermFromString((String) courseAddTermStrings.get(position));
-                    associatedTerms.add(courseAddTermSelected);
-                    associatedTermIds.add(courseAddTermSelected.getTermId());
-                    populateFields();
-                }
-                break;
-            case R.id.courseDeleteTermSpinner:
-                if (position != 0){
-                    Term courseDeleteTermSelected = repo.getTermFromString((String) courseDeleteTermStrings.get(position));
-                    associatedTerms.remove(position - 1);
-                    associatedTermIds.remove(courseDeleteTermSelected.getTermId());
-                    populateFields();
-                }
                 break;
             case R.id.courseAddAssessmentSpinner:
                 if (position != 0){
@@ -237,22 +220,14 @@ public class CourseActivity extends AppCompatActivity implements AdapterView.OnI
                 mCourse.setInstructorPhone(coursePhoneEditText.getText().toString());
                 mCourse.setInstructorEmail(courseEmailEditText.getText().toString());
                 mCourse.setCourseNotes(courseNotesEditText.getText().toString());
-                if (mCourse.getCourseId() != null){
-                   /* for (Integer i : associatedTermIds){
-                        if(!repo.getAssociatedTerms(mCourse).contains(i)){
-                            Term term = repo.getTermById(i);
-                            List<Integer> assCoursesTemp = term.getAssociatedCourses();
-                            term.setTermCourses(assCoursesTemp);
-                            repo.update(term);
-                        };
+                if (validateFields()){
+                    if (mCourse.getCourseId() != null){
+                        repo.update(mCourse);
+                    } else {
+                        repo.insert(mCourse);
                     }
-
-                    */
-                    repo.update(mCourse);
-                } else {
-                    repo.insert(mCourse);
+                    startActivity(intent);
                 }
-                startActivity(intent);
                 return true;
             case R.id.delete:
                 Log.d(LOG_ID, "You clicked delete!");
@@ -267,95 +242,13 @@ public class CourseActivity extends AppCompatActivity implements AdapterView.OnI
 
     public void populateFields(){
         //First check to see if we are editing an existing course or making a new one
-        courseExecutor.execute(()-> {
-            if (mCourse.getCourseId() != null && associatedTermIds.isEmpty()) {
-                //Find our associated terms
-                associatedTermIds = repo.getAssociatedTerms(mCourse);
-                //associatedTermIds = new ArrayList<>();
-                for (Integer i : associatedTermIds) {
-                    associatedTerms.add(repo.getTermById(i));
-                }
-                //Find our associated assessments
-                associatedAssessmentIds = mCourse.getAssociatedAssessments();
-                for (Integer i : associatedAssessmentIds) {
-                    associatedAssessments.add(repo.getAssessmentById(i));
-                }
-            }
-        });
-
-        //Take that associated Term information and populate the terms recyclerview
-        RecyclerView courseAssociatedTermsRecyclerView = findViewById(R.id.courseDetailTermsRecyclerView);
-        final TermAdaptor termAdaptor = new TermAdaptor(CourseActivity.this);
-        courseAssociatedTermsRecyclerView.setAdapter(termAdaptor);
-        courseAssociatedTermsRecyclerView.setLayoutManager(new LinearLayoutManager(CourseActivity.this));
-        termAdaptor.setTerms(associatedTerms);
-
-        //Populate the Add and Delete Spinners for Terms
-        courseAddTermStrings = new ArrayList<>();
-        courseDeleteTermStrings = new ArrayList<>();
-        courseAddTermStrings.add("Add");
-        courseDeleteTermStrings.add("Delete");
-        List<Term> allTerms = repo.getAllTerms();
-        for (Term t : allTerms) {
-            if (associatedTermIds.contains(t.getTermId())) {
-                courseDeleteTermStrings.add(t.toString());
-            } else {
-                courseAddTermStrings.add(t.toString());
+        if (mCourse.getCourseId() != null && associatedAssessmentIds.isEmpty()) {
+            //Find our associated assessments
+            associatedAssessmentIds = mCourse.getAssociatedAssessments();
+            for (Integer i : associatedAssessmentIds) {
+                associatedAssessments.add(repo.getAssessmentById(i));
             }
         }
-        Spinner courseAddTermSpinner = findViewById(R.id.courseAddTermSpinner);
-        ArrayAdapter<String> courseTermAddAdapter = new ArrayAdapter<String>(CourseActivity.this, android.R.layout.simple_spinner_dropdown_item, courseAddTermStrings) {
-            @Override
-            public boolean isEnabled(int position) {
-                if (position == 0) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-
-            @Override
-            public View getDropDownView(int position, View convertView, ViewGroup parent) {
-                View view = super.getDropDownView(position, convertView, parent);
-                TextView textview = (TextView) view;
-                if (position == 0) {
-                    textview.setTextColor(Color.WHITE);
-                    textview.setBackgroundColor(Color.BLACK);
-                } else {
-                    textview.setTextColor(Color.BLACK);
-                }
-                return view;
-            }
-        };
-        courseAddTermSpinner.setAdapter(courseTermAddAdapter);
-        courseAddTermSpinner.setOnItemSelectedListener(CourseActivity.this);
-
-        Spinner courseDeleteTermSpinner = findViewById(R.id.courseDeleteTermSpinner);
-        ArrayAdapter<String> courseDeleteTermAdapter = new ArrayAdapter<String>(CourseActivity.this, android.R.layout.simple_spinner_dropdown_item, courseDeleteTermStrings) {
-            @Override
-            public boolean isEnabled(int position) {
-                if (position == 0) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-
-            @Override
-            public View getDropDownView(int position, View convertView, ViewGroup parent) {
-                View view = super.getDropDownView(position, convertView, parent);
-                TextView textview = (TextView) view;
-                if (position == 0) {
-                    textview.setTextColor(Color.WHITE);
-                    textview.setBackgroundColor(Color.BLACK);
-                } else {
-                    textview.setTextColor(Color.BLACK);
-                }
-                return view;
-            }
-        };
-        courseDeleteTermSpinner.setAdapter(courseDeleteTermAdapter);
-        courseDeleteTermSpinner.setOnItemSelectedListener(CourseActivity.this);
 
         //populate assessment RecyclerView
         RecyclerView courseAssociatedAssessmentsRecyclerView = findViewById(R.id.courseDetailAssessmentsRecyclerView);
@@ -431,6 +324,41 @@ public class CourseActivity extends AppCompatActivity implements AdapterView.OnI
         courseDeleteAssessmentSpinner.setAdapter(courseDeleteAssessmentAdapter);
         courseDeleteAssessmentSpinner.setOnItemSelectedListener(CourseActivity.this);
 
+    }
+
+    public Boolean validateFields() {
+        Boolean validated = true;
+        String errorMessage = "";
+
+        //Check that Start Date is earlier than End Date
+        if(mCourse.getCourseStart().isAfter(mCourse.getCourseEnd())){
+            validated = false;
+            errorMessage = errorMessage + "Start date is after End Date. \n";
+        }
+
+        //Check that the phone number matches expected patterns
+        String phoneRegex = "\\d{10}|(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}";
+        if(!mCourse.getInstructorPhone().matches(phoneRegex)){
+            validated = false;
+            errorMessage = errorMessage + "Phone number is not entered correctly. \n";
+        }
+
+        //Check that the email matches expected patterns
+        String emailRegex = "^[\\w!#$%&amp;'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&amp;'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+        if(!mCourse.getInstructorEmail().matches(emailRegex)){
+            validated = false;
+            errorMessage = errorMessage + "Email address is not entered correctly. ";
+        }
+
+        if(!validated) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(CourseActivity.this);
+            builder.setTitle("Error Saving")
+                    .setMessage(errorMessage)
+                    .create()
+                    .show();
+        }
+
+        return validated;
     }
 
 }
